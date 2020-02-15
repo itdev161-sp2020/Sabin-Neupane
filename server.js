1686b78fad535d8ed0257a32bc59ff09ecc5330e
neupanesabin <54862359+neupanesabin@users.noreply.github.com>
2020-02-14 18:07:44 -0600

    Added database connection

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import connectionDatabase from './config/db';
 
 const app= express();
+//connect database
+connectionDatabase();
+
 app.get('/',(req,res)=>
 res.send('http get request sent to root api endpoint')
 );
